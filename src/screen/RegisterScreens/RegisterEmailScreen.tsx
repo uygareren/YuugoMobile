33ef6916517f275227c990855316558f2564ae4d
Uygar <komifb90@hotmail.com>
2024-06-07 18:19:03 +0300

    registerEmailScreen is ok

--- src/screen/RegisterScreens/RegisterEmailScreen.tsx
+++ src/screen/RegisterScreens/RegisterEmailScreen.tsx
@@ -1,13 +1,15 @@
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { Formik } from "formik";
-import { Button, useTheme } from "native-base";
+import { useTheme } from "native-base";
 import { Dimensions, Text, View } from "react-native";
 import * as yup from "yup";
+import { ButtonComp } from "../../components/ButtonComp";
 import TextInput from "../../components/input/TextInput";
 import { useI18n } from "../../hooks/useI18n";
 import { RootStackParamList } from "../../types/react-navigation";
 import i18n from "../../utils/i18n/i18n";
+import { BUTTON_RADIUS } from "../../utils/utils";
 
 type RegisterEmailScreenNavigationProp = NativeStackNavigationProp<
     RootStackParamList,
@@ -26,13 +28,13 @@ export default function RegisterEmailScreen(){
 
 
     function handleConfirmEmail(){
-
+        navigation.navigate("RegisterConfirmCode");
     }
 
     return(
         <View style={{backgroundColor: theme.colors.white, flex:1, paddingHorizontal:16, justifyContent:"center"}}>
 
-            <View style={{position:"absolute", top:64, paddingHorizontal:16, borderWidth:1, width:width}}>
+            <View style={{position:"absolute", top:64, paddingHorizontal:16, width:width}}>
 
                 <View style={{flexDirection:"row", marginTop:16}}>
                     {/* ARROW LEFT İCON
@@ -40,11 +42,11 @@ export default function RegisterEmailScreen(){
                         
                     </TouchableOpacity> */}
                     <View>
-                        <Text style={{fontSize:24, color:theme.colors.black, fontWeight:"500"}}>Email Confirm!</Text>
+                        <Text style={{fontSize:24, color:theme.colors.black, fontWeight:"500"}}>{t("emailConfirm")}</Text>
                     </View>
                 </View>
                 <View style={{marginTop:16}}>
-                    <Text style={{fontSize:16, fontWeight:"500", color:theme.colors.black}}>Enter your email to register</Text>
+                    <Text style={{fontSize:16, fontWeight:"300", color:theme.colors.black}}>{t("emailConfirmSubText")}</Text>
                 </View>
 
             </View>
@@ -57,17 +59,20 @@ export default function RegisterEmailScreen(){
             onSubmit={handleConfirmEmail}
             >
                 {({errors, touched, values, handleChange, handleBlur, handleSubmit}) => (
+                    console.log("error", errors),
                     <>
                     <View style={{marginTop:45}}>
                     <TextInput label={t("email")} value={values.email} onChangeText={handleChange("email")}
-                                onBlur={handleBlur("email")}
-                                required isInvalid={errors.email != undefined && touched.email as boolean}
-                                errorMessage={errors.email} />
-                    <Button onPress={() => handleSubmit()} mt="20px" py="20px"
-                        alignSelf="center" width={width - 32} mx="16px" marginBottom="14px" 
-                        >
-                            Emaili Doğrula
-                    </Button>
+                    onBlur={handleBlur("email")}
+                    required isInvalid={errors.email != undefined && touched.email as boolean}
+                    errorMessage={errors.email} />
+
+                    <ButtonComp onPress={handleSubmit} 
+                        containerStyle={{marginTop:32, paddingVertical:16, alignItems:"center", justifyContent:"center",
+                        borderRadius:BUTTON_RADIUS, backgroundColor:"blue"}}
+                        textStyle={{color:theme.colors.white, fontWeight:"600", fontSize:16}}
+                        loading={false}/>
+                    
 
                     </View>
                     </>
